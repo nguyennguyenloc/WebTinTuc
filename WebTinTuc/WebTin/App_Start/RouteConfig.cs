f21ea1d043dae3772ccee017432dd44d117a481e
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-03-27 06:25:00 +0700

    Update data layer

--- WebTinTuc/WebTin/App_Start/RouteConfig.cs
+++ WebTinTuc/WebTin/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace WebTin
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new string[] {"WebTin.Controllers"}
+                namespaces: new [] {"WebTin.Controllers"}
             );
         }
     }
--- WebTinTuc/WebTin/Areas/Admin/AdminAreaRegistration.cs
+++ WebTinTuc/WebTin/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebTin.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new [] {"WebTin.Areas.Admin.Controllers"}
             );
         }
     }
